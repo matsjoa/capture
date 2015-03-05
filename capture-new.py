83a504f64a75d39d1eb25a28c5c3594d1141f08a
Mats Johannesen <mats@matsj.net>
2015-03-05 10:26:32 +0100

    Added capture

---
+++ capture-new.py
@@ -0,0 +1,16 @@
+from sys import argv
+import cv2
+
+script, videofile = argv
+
+videocap = cv2.VideoCapture("Filter-test.mp4")
+
+success,image = videocap.read()
+
+count = 0;
+while success:
+    success,image = videocap.read()
+    cv2.imwrite("frame%d.jpg" % count, image)   # Save frame as JPEG file
+    if cv2.waitKey(10) == 27:                   # Exit if Escape is hit
+        break
+    count += 1
